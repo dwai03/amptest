de96effd19d3c5513497f768cece0f023da38cc2
Christina Kiesz <ckiesz@gmail.com>
2021-02-20 11:36:38 -0800

    add nodenv, readme, app page

---
+++ readme.md
@@ -0,0 +1,7 @@
+# Facets Webapp v0.1
+
+- Nodenv version control - Node 14.15.4
+- Yarn package manager
+- Webpack to bundle the js modules together
+- Webpack dev server for local development
+- Babel to transpile the js into browser compatible versions
--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
-import React, { Component} from "react";
-import "./App.css";
+import React from 'react';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import Link from '@material-ui/core/Link';
+import ProTip from './ProTip';
 
-class App extends Component{
-  render(){
-    return(
-      <div className="App">
-        <h1> Hello, World! </h1>
-      </div>
-    );
-  }
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://material-ui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
 }
 
-export default App;
+export default function App() {
+  return (
+    <Container maxWidth="sm">
+      <Box my={4}>
+        <Typography variant="h4" component="h1" gutterBottom>
+          Create React App v4-beta example
+        </Typography>
+        <ProTip />
+        <Copyright />
+      </Box>
+    </Container>
+  );
+}
