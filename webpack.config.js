afaf8ba0f65ec89b9bd565054b3b57b0aa5991aa
Christina Kiesz <ckiesz@gmail.com>
2021-02-20 10:52:51 -0800

    working initial webpack of js

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    module: {
+        rules: [
+          {
+            test: [/\.jsx?$/],
+            exclude: /(node_modules)/,
+            loader: 'babel-loader',
+            options: {
+                presets: ['@babel/env', '@babel/react']
+            },
+          },
+          {
+            test: /\.css$/,
+            use: ["style-loader", "css-loader"]
+          }
+        ]
+      },
+      devtool: 'source-map',
+      resolve: {
+        extensions: ['.js', '.jsx', '*']
+      },
+      output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+      devServer: {
+        contentBase: path.join(__dirname, "public/"),
+        port: 3000,
+        publicPath: "http://localhost:3000/dist/",
+      },
+  };
+  
