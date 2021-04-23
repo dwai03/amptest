51fb1c7adb85bfcac99523a8c96f86f0e0f0d5c5
Dave <citizen@dave.local>
2021-04-22 23:58:33 -0400

    user state

--- frontend/src/components/common/Header.js
+++ frontend/src/components/common/Header.js
@@ -1,15 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import _ from "lodash";
 import Logo from "./facetslogo.png";
 import "../../stylesheets/layout.scss";
 
 const navs = {
-  HowItWorks: { display: "How It Works", url: "/" },
-  Agencies: { display: "Brands & Agencies", url: "/" },
-  Learn: { display: "Learn", url: "/" },
-  Pricing: { display: "Pricing", url: "/" }
+  HowItWorks: { display: "How It Works", url: "/", type: "default" },
+  Agencies: { display: "Brands & Agencies", url: "/", type: "default" },
+  Learn: { display: "Learn", url: "/", type: "all" },
+  Pricing: { display: "Pricing", url: "/", type: "default" },
+  WorkFlow: { display: "Work Flow", url: "/", type: "creator" },
+  DashBoard: { display: "Dashboard", url: "/", type: "creator" }
 };
 const NavBar = props => {
+  const [user, setUser] = useState();
+  useEffect(() => {
+    const loggedInUser = localStorage.getItem("user");
+    if (loggedInUser) {
+      const foundUser = JSON.parse(loggedInUser);
+      setUser(foundUser);
+    }
+  }, []);
   return (
     <div className="nav-bar">
       <div className="nav-logo">
@@ -17,18 +27,44 @@ const NavBar = props => {
       </div>
       <div className="nav-links">
         <div className="nav-pages">
-          {_.map(navs, ({ display, url }, k) => (
-            <a key={k} href={url}>
-              {display}
-            </a>
-          ))}
+          {user &&
+            _.map(
+              navs,
+              ({ display, url, type }, k) =>
+                (type == "creator" || type == "all") && (
+                  <a key={k} href={url}>
+                    {display}
+                  </a>
+                )
+            )}
+          {!user &&
+            _.map(
+              navs,
+              ({ display, url, type }, k) =>
+                (type == "default" || type == "all") && (
+                  <a key={k} href={url}>
+                    {display}
+                  </a>
+                )
+            )}
         </div>
-        <div className="sign-in-btn">
-          <a>Already a creator? Sign in</a>
-          <button>
-            <span>BECOME A CREATOR</span>
-          </button>
-        </div>
+        {!user && (
+          <div className="sign-in-btn">
+            <a>Already a creator? Sign in</a>
+            <button>
+              <span>BECOME A CREATOR</span>
+            </button>
+          </div>
+        )}
+        {user && (
+          <div className="sign-in-btn">
+            <button>
+              <span>
+                {user.firstName} {user.lastName}
+              </span>
+            </button>
+          </div>
+        )}
       </div>
     </div>
   );
--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,11 +1,25 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "../stylesheets/module.scss";
 export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [user, setUser] = useState();
   async function handleSubmit(e) {
     const user = { email, password };
-    debugger;
+    // const response = await axios.post("http://localhost:3000/api/login", user);
+    let response = {
+      data: {
+        username: "themisterjuly",
+        firstName: "Andrew",
+        lastName: "Scott",
+        categories: ["Beauty", "Fashion", "Fitness"]
+      }
+    };
+    setUser(response.data);
+    // store the user in localStorage
+    localStorage.setItem("user", JSON.stringify(response.data));
+    console.log(response.data);
   }
   return (
     <div className="login-container">
